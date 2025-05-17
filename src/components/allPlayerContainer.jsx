f52a97930d7761e3d3d6f3e5de2c12bd2a30ad64
mehedihasan101101 <sojibkhan39621@gmail.com>
2025-05-17 14:11:18 +0100

    small fix

--- src/components/allPlayerContainer.jsx
+++ src/components/allPlayerContainer.jsx
@@ -33,7 +33,7 @@ export default function AllPlayerContainer({ freeCoin, setFreeCoin }) {
     return (
         <>
             <div id="allPlayers" className="flex justify-between items-center sticky top-25 z-50  ">
-                <h3 className=" font-bold md:text-2xl ">{isClicked == true ? "Available Players" : `Selected Player (${selectedPlayerId.length}/11) `}</h3>
+                <h3 className=" font-bold md:text-2xl ">{isClicked == true ? "Available Players" : `Selected Players (${selectedPlayerId.length}/11) `}</h3>
                 <div className="h-full  w-fit rounded-lg  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] ">
                     <button onClick={btnAvalAble} className={`rounded-tl-lg rounded-bl-lg  py-2 px-5 cursor-pointer md:text-[18px] text-[12px]
                     ${isClicked == true ? "bg-[#E7FE29]" : "bg-white"}`}  >Available</button>
